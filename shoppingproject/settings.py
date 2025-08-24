3624e94051527deb91fd6cddeca3a20be399d2f2
vigneshanandhu <vigneshanandhu2003@gmail.com>
2025-08-24 22:26:09 +0530

    ninth update

--- shoppingproject/settings.py
+++ shoppingproject/settings.py
@@ -124,7 +124,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'shoppingapp/static'),
+]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
