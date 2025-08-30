5f0c7df31ff71c73b61e73a61d4ff1a0877c916b
vigneshanandhu <vigneshanandhu2003@gmail.com>
2025-08-30 09:25:17 +0530

    sixteen update

--- shoppingproject/settings.py
+++ shoppingproject/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = config('SECRET_KEY', default='django-insecure-_f##f9%!vi2t9w@6-7i3a
 DEBUG = config('DEBUG', default=False, cast=bool)
 
 # Update ALLOWED_HOSTS for production
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS','localhost,127.0.0.1,,viki-s-shop.onrender.com').split(',')
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS','localhost,127.0.0.1,viki-s-shop.onrender.com').split(',')
 
 
 # Application definition
